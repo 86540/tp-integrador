a3b568f0ebf38ebe2ed5128afaf8855e6ad56e1f
Alejo Marcantoni <alejomarcantoni10@gmail.com>
2023-04-13 00:32:33 -0300

    comit nuevo
    nuevos cambios en main.java, partido.java y ronda.java

--- untitledtp/src/ronda.java
+++ untitledtp/src/ronda.java
@@ -1,4 +1,16 @@
 import java.util.ArrayList;
+public class ronda{
+
+    public ArrayList<partido> partidos = new ArrayList<partido>();
+    public void ListarGanadores()
+    {
+        for (int i = 0; i < partidos.size() ; i++) {
+            System.out.println("el ganador del partido fue :"+partidos.get(i).getGanador());
+        }
+        System.out.println("--------------------------------");
+    }
+}
+/*import java.util.ArrayList;
 //<<<<<<< HEAD
 public class Ronda {
     Partido[] partidoss = new Partido[4];
@@ -16,3 +28,4 @@ public class ronda{
     }
 }
 }
+*/
