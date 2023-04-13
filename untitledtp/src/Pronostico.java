7ff0781784281c367695729d157a75f976ed2f9b
Alejo Marcantoni <alejomarcantoni10@gmail.com>
2023-04-12 22:13:10 -0300

    Update Pronostico.java

--- untitledtp/src/Pronostico.java
+++ untitledtp/src/Pronostico.java
@@ -1,24 +1,33 @@
-public class Pronostico {
-    private Partido partido;
-    private Equipo equipo;
-    //private String getGanador();
-    
+public class pronostico {
+ public pronostico(String equipo1,String gana1,String empate,String gana2,String equipo2,String apostador){
+  this.equipo1 = equipo1;
+  this.equipo2 = equipo2;
+  this.empate = empate;
+  this.apostador= apostador;
+  this.gana1=gana1;
+  this.gana2=gana2;
+ }
+ private String equipo1;
+ private String empate;
+ private String equipo2;
+ private String apostador;
+ private String gana1;
+ private String gana2;
+ public String getapostador(){return apostador; }
+ public String getequipo1(){return equipo1;}
+ public String getempate(){return empate; }
+ public String getequipo2(){return equipo2; }
+ public String getapuesta(){
+  if (gana1.equals("x")){
+   return equipo1;
+  }
+  if (gana2.equals("x")){
+   return equipo2;
+  }
+  else{
+   return "empate";
+  }
+ }
 
-    public Pronostico(Partido partido, Equipo equipo){ //String getGanador()) {
-        this.partido=partido;
-        this.equipo=equipo;
-        //this.getGanador()=getGanador();
-    }
-
-
-public Partido getPartido() {return partido;}
-public Equipo getEquipo() {return equipo;}
-//public String getganador() {return getGanador();}
-public String getGanador() {return getGanador();}
-public int puntos(){
-    String resultado_real=partido.getGanador(this.equipo);
-    if(getGanador().equals(resultado_real)) {return 1;}
-    else {return 0;}
-}
 
 }
